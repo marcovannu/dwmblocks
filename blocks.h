1f7447e75eaf6a968c5043b0a858619e9099121f
Marco Vannucci <53081969+marcovannu@users.noreply.github.com>
2023-12-21 10:11:23 +0100

    Add files via upload

--- blocks.h
+++ blocks.h
@@ -1,29 +1,32 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"📦 ", "checkupdates | wc -l",					1200,		9},
 
-	{"", "~/.local/bin/statusbar/news",				0,		6},
+	{"📦 ", "checkupdates | wc -l",			1200		,9},
 
-	{"", "~/.local/bin/statusbar/mailbox",				180,		18},
+	/*{"", "~/.local/bin/statusbar/news",		0,		6},*/
 
-	{"", "~/.local/bin/statusbar/nettraf",				1,		11},
+	/*{"", "~/.local/bin/statusbar/mailbox",	180,		18},*/
 
-	{"", "ip route get 1 | awk '{print $(NF-2);exit}'",		1200,		12},
+	{"", "~/.local/bin/statusbar/nettraf",		2,		11},
 
-	{"", "~/.local/bin/statusbar/internet",				5,		4},
+	{"", "~/.local/bin/statusbar/bluetooth",	10,		13},
 
+	/*{"", "ip route get 1 | awk '{print $(NF-2);exit}'",		1200,		12},*/
 
-	/*{"", "~/.local/bin/statusbar/capture",				0,		2},*/
+	{"", "~/.local/bin/statusbar/internet",		10,		4},
 
-	{"", "~/.local/bin/statusbar/volume",				0,		10},
+	/*{"", "~/.local/bin/statusbar/capture",	0,		2},*/
+
+	{"", "~/.local/bin/statusbar/volume",		0,		10},
 
 	/*{"-[] ", "acpi -b | grep -o 'Charging' | sed 's/Charging/~/' && echo $(acpi -b | grep -o '[0-9]*%')",						10,		0},*/
-	{"", "~/.local/bin/statusbar/battery",				5,		3},
+	{"", "~/.local/bin/statusbar/battery",		10,		3},
 
 	/*{"🌡 ", "sensors | awk '/^temp1:/{print $2}'",		5,		0},*/
 
-	{"", "~/.local/bin/statusbar/clock",				60,		1},
+	{"", "~/.local/bin/statusbar/clock",		60,		1},
+
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
